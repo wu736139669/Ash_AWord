3ca13ba6571412b83bd080bc7ab3ab5b836f0044
wu736139669 <wu736139669@736139669@qq.com>
2015-05-15 10:52:58 +0800

    修改iOS6.0bug

--- Ash_AWord/ViewControllers/Main/MainViewController.m
+++ Ash_AWord/ViewControllers/Main/MainViewController.m
@@ -30,7 +30,22 @@
             
             [_messageNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"听你说" image:[UIImage imageNamed:@"tabbarVoice-dark"] selectedImage:[UIImage imageNamed:@"tabbarVoiceClick"]]];
             
-            [_myNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"我的" image:[UIImage imageNamed:@"tabbarSetup-dark"] selectedImage:[UIImage imageNamed:@"tabbarSetupClick"]]];
+            [_myNav setTabBarItem:[[UITabBarItem alloc] initWithTitle:@"我" image:[UIImage imageNamed:@"tabbarSetup-dark"] selectedImage:[UIImage imageNamed:@"tabbarSetupClick"]]];
+        }else{
+            UITabBarItem* item1 = [[UITabBarItem alloc] init];
+            [item1 setFinishedSelectedImage:[UIImage imageNamed:@"tabbarQuotationClick"] withFinishedUnselectedImage:[UIImage imageNamed:@"tabbarQuotation-dark"]];
+            item1.title = @"遇见你";
+            
+            _noteNav.tabBarItem = item1;
+            UITabBarItem* item2 = [[UITabBarItem alloc] init];
+            [item2 setFinishedSelectedImage:[UIImage imageNamed:@"tabbarVoiceClick"] withFinishedUnselectedImage:[UIImage imageNamed:@"tabbarVoice-dark"]];
+            item2.title = @"听你说";
+            
+            _messageNav.tabBarItem = item2;
+            UITabBarItem* item3 = [[UITabBarItem alloc] init];
+            [item3 setFinishedSelectedImage:[UIImage imageNamed:@"tabbarSetupClick"] withFinishedUnselectedImage:[UIImage imageNamed:@"tabbarSetup-dark"]];
+            item3.title = @"我";
+            _myNav.tabBarItem = item3;
         }
         
         self.viewControllers = @[_noteNav, _messageNav, _myNav];
