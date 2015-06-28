9b620e52061821e7b6f886ccadee9f0f3e6f6220
wu736139669 <wu736139669@736139669@qq.com>
2015-06-28 13:52:30 +0800

    new

---
+++ Ash_AWord/Views/Common/MessageSetTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MessageSetTableViewCell.h
+//  Ash_AWord
+//
+//  Created by xmfish on 15/6/28.
+//  Copyright (c) 2015年 ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageSetTableViewCell : UITableViewCell
+
+@end
---
+++ Ash_AWord/Views/Common/MessageSetTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MessageSetTableViewCell.m
+//  Ash_AWord
+//
+//  Created by xmfish on 15/6/28.
+//  Copyright (c) 2015年 ash. All rights reserved.
+//
+
+#import "MessageSetTableViewCell.h"
+
+@implementation MessageSetTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
